d767018eaf0420023d46238653c479ed24792e51
MadnessFreak <supervivo@icloud.com>
2014-11-19 15:00:01 +0100

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('APP_DIR')) define('APP_DIR', dirname(__FILE__) .'/');
+if (!defined('RELATIVE_APP_DIR')) define('RELATIVE_APP_DIR', '');
+
+if (!defined('SYS_DIR')) define('SYS_DIR', dirname(__FILE__) . '/mata/');
+if (!defined('RELATIVE_SYS_DIR')) define('RELATIVE_SYS_DIR', RELATIVE_APP_DIR . 'mata/');
+
+if (!defined('ENABLE_DEBUG_MODE')) define('ENABLE_DEBUG_MODE', 1);
