8530216e857ca07a0fb698c2fb3f26dadff21962
MadnessFreak <supervivo@icloud.com>
2014-11-19 14:58:26 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Matavior
+ * 
+ * @author    MadnessFreak <madnessfreak@happyduckz.co>
+ * @copyright	2014 MadnessFreak
+ * @package   Matavior
+ */
+
+// include config
+require_once(dirname(__FILE__) . '/config.inc.php');
+
+// include system
+require_once(RELATIVE_SYS_DIR . 'Mata.php');
+
+// initialize
+Mata::init();
