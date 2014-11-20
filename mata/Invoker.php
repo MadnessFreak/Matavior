78ced3a3541d234d0d5a67457160c5e53ca71ae3
MadnessFreak <supervivo@icloud.com>
2014-11-20 09:22:48 +0100

    Create Invoker.php

---
+++ mata/Invoker.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Provides a class for invoking a controller.
+ * 
+ * @author    MadnessFreak <madnessfreak@happyduckz.co>
+ * @copyright 2014 MadnessFreak
+ * @package   Matavior
+ */
+class Invoker {
+	/**
+	 * Invokes the specified controller.
+	 *
+	 * @param	string	name
+	 */
+	public static function invoke($name) {
+		// create controller name
+		$className = ucfirst($name) . 'Controller';
+		$path = Utility::unidir(SYS_DIR . 'Controller/' . $className . '.php');
+		
+		// try invoke
+		if (file_exists($path)) {
+			$controller = new $className();
+			$controller->init();
+		} else {
+			if (Mata::debugModeIsEnabled()) echo "Invoking $name failed. Could not find $path.<br>";
+		}
+	}
+}
