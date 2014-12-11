4b293b17066b2ff7bcb95c853e9c790f949b907f
MadnessFreak <supervivo@icloud.com>
2014-12-11 18:35:28 +0100

    Add cookie and session defines

--- mata/Config/options.inc.php
+++ mata/Config/options.inc.php
@@ -4,5 +4,11 @@ if (!defined('PAGE_TITLE')) define('PAGE_TITLE', 'Matavior');
 if (!defined('PAGE_AUTHOR')) define('PAGE_AUTHOR', 'MadnessFreak');
 if (!defined('PAGE_KEYWORDS')) define('PAGE_KEYWORDS', 'Matavior, Mata, Framework, Free, Open Source, PHP');
 if (!defined('PAGE_DESCRIPTION')) define('PAGE_DESCRIPTION', 'Matavior is a little free web-framework.');
-
+if (!defined('DEFAULT_LANGUAGE')) define('DEFAULT_LANGUAGE', 'en');
 if (!defined('DB_PREFIX')) define('DB_PREFIX', 'mata');
+
+if (!defined('SESSION_TIMEOUT')) define('SESSION_TIMEOUT', 450); // 7.5 min
+
+if (!defined('COOKIE_SESSION')) define('COOKIE_SESSION', 'mata_session');
+if (!defined('COOKIE_LANGUAGE')) define('COOKIE_LANGUAGE', 'mata_lang');
+if (!defined('COOKIE_LOGIN_REMEMBER')) define('COOKIE_LOGIN_REMEMBER', 'mata_remember');
